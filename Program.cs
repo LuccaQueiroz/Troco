a468dce141fa9a6cc2902fc89197d24214374377
Lucas <lucasqueirozzwerner@gmail.com>
2022-09-17 11:00:20 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("-----Troco da compra-----\n");
+
+int valorCompra, valorRecebido, troco;
+ 
+Console.Write("Valor total da compra: ");
+valorCompra = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Valor recebido: ");
+valorRecebido = Convert.ToInt32(Console.ReadLine());
+
+troco = valorRecebido - valorCompra;
+
+Console.WriteLine($"Troco: R${troco}");
